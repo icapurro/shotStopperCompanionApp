8a4352de6608bc7fb38391d132285bdfe308aed5
Ignacio Capurro <ignaciocapurro@Ignacios-MacBook-Pro.local>
2025-03-05 23:18:24 -0300

    Initial setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,75 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
+import React from 'react';
 import { Stack } from 'expo-router';
+import { ThemeProvider, useThemeContext } from './hooks/useThemeContext';
+import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { DeviceSettingsProvider } from './hooks/DeviceSettingsContext';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+const AppContent = () => {
+  const { theme, isDarkMode } = useThemeContext();
+  
+  return (
+    <>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: theme.colors.background,
+          },
+          headerTintColor: theme.colors.text,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          contentStyle: { 
+            backgroundColor: '#111110'
+          },
+          animation: 'fade',
+          freezeOnBlur: true,
+          animationDuration: 400
+        }}
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="home"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="settings"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+      <StatusBar style={isDarkMode ? 'light' : 'dark'} />
+    </>
+  );
+};
+
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
+  const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    MenloRegular: require('../assets/fonts/Menlo-Regular.ttf'),
+    MenloBold: require('../assets/fonts/Menlo-Bold.ttf'),
+    InstrumentSherif: require('../assets/fonts/InstrumentSerif-Regular.ttf'),
   });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
 
   useEffect(() => {
     if (loaded) {
@@ -28,12 +82,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider>
+        <DeviceSettingsProvider>
+          <SafeAreaProvider>
+            <AppContent />
+          </SafeAreaProvider>
+        </DeviceSettingsProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,25 +2,46 @@
  * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
+import { DefaultTheme, DarkTheme } from '@react-navigation/native';
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#E44C25';
+const tintColorDark = '#E9E7E0';
 
-export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
+
+const MyLightTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    text: '#111110',
+    background: '#E9E7E0',
+    altBackground: '#DEDCD3',
+    card: '#DEDCD3',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
+    black: "#111110",
+    primary: "#E44C25",
+    white: '#E9E7E0',
   },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+};
+
+const MyDarkTheme = {
+  ...DarkTheme,
+  colors: {
+    ...DarkTheme.colors,
+    text: '#E9E7E0',
+    background: '#111110',
+    altBackground: '#2D2B28',
+    card: '#2D2B28',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorDark,
+    black: "#111110",
+    primary: "#E44C25",
+    white: '#E9E7E0',
   },
 };
+
+export { MyLightTheme, MyDarkTheme };
